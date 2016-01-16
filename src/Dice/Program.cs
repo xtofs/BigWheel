2407582e1da15ef97951c95bc9fb93136c4178f2
Christof Sprenger <christof_sprenger@hotmail.com>
2016-01-16 15:15:21 -0800

    add more samples

--- src/Dice/Program.cs
+++ src/Dice/Program.cs
@@ -1,45 +1,71 @@
 ﻿using System;
 using System.Linq;
 
-namespace Statistics
+namespace Xof.RandomVariables
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var twoDice =
-              from d1 in Random.Dice(6)
-              from d2 in Random.Dice(6)
-              select d1 + d2;
-            Show("two dice", twoDice); 
+            //var twoDice =
+            //  from d1 in 
+            //  from d2 in Random.Dice(6)
+            //  select d1 + d2;
+            Demo("two dice", Random.Dice(6).Repeat(2).Select(lst => lst.Sum()));
 
-            // dice roles from teh rules of Zombicide
+            // dice roles from the rules of Zombicide
+            // Zomicide();
 
+            DnD();
+        }
+
+        private static void DnD()
+        {
+            var plus10Vsplus10 =
+              from a in Random.Dice(20)
+              from d in Random.Dice(20)              
+              select a + 10 >= d + 10;
+            Demo("+10 attacks +10", plus10Vsplus10);
+        }
+
+
+        // dice roles from the rules of Zombicide
+        private static void Zomicide()
+        {
+        
             var threeDiceThreePlus =
                  from d1 in Random.Dice(6)
                  from d2 in Random.Dice(6)
                  from d3 in Random.Dice(6)
                  select new[] { d1, d2, d3 }.Count(d => d >= 3);
-            Show("three dice 3+", threeDiceThreePlus);
+            Demo("number of dice out of 3, that have value >= 3 (three dice 3+)", threeDiceThreePlus);
 
-            var threeDiceThreePlusTwoDamage =
-              from ds in Random.Dice(6).Repeat(3)
-              select ds.Where(d => d >= 4).Select(d => 2).Sum();
-            Show("four dice 4+, 2 damage", threeDiceThreePlusTwoDamage);
+            var threeDiceFourPlus =
+                from ds in Random.Dice(6).Repeat(3)
+                select ds.Count(d => d >= 4);
+            Demo("three dice 4+", threeDiceFourPlus);
 
             var fourDiceFourPlus =
-             from ds in Random.Dice(6).Repeat(4)
-             select ds.Count(d => d >= 4);
-            Show("four dice 4+", fourDiceFourPlus);
+                from ds in Random.Dice(6).Repeat(4)
+                select ds.Count(d => d >= 4);
+            Demo("four dice 4+", fourDiceFourPlus);
+
+            var threeDiceThreePlusTwoDamage =
+                from ds in Random.Dice(6).Repeat(3)
+                select ds.Where(d => d >= 4).Select(d => 2).Sum();
+            Demo("four dice 4+, 2 damage", threeDiceThreePlusTwoDamage);
+
 
             var threeDiceThreePlusWithOnes =
                 from ds in Random.Dice(6).Repeat(3)
                 let hits = ds.Count(d => d >= 3)
                 select new { Hits = hits, Ones = Math.Min(hits, ds.Count(d => d == 1)) };
-            Show("three dice 3+ show 1's count", threeDiceThreePlusWithOnes, p => string.Format("{0}/{1}", p.Hits, p.Ones));
+            Demo("three dice 3+ with count of 1's", threeDiceThreePlusWithOnes, p => string.Format("{0} - {1}", p.Hits, p.Ones));
         }
 
-        private static void Show<T>(string title, IRandom<T> random, Func<T, string> fmt = null)
+        private static System.Random rand = new System.Random();
+
+        private static void Demo<T>(string title, IRandom<T> random, Func<T, string> fmt = null, int m = 10, int n = 1000000)
         {
             fmt = fmt ?? (t => t.ToString());
 
@@ -47,18 +73,22 @@ namespace Statistics
             Console.WriteLine("# {0}", title);
             foreach (var outcome in random)
             {
-                Console.WriteLine("{0,2} {1,7:P2}", fmt(outcome.Key), outcome.Value);
+                Console.WriteLine("{0,4} {1,7:P2}", fmt(outcome.Key), outcome.Value);
             }
             Console.WriteLine();
 
-            var rand = new System.Random();
-            Console.WriteLine(random.Sample(rand).Take(10).Format(" ", "{0}", p => fmt(p)));
+            Console.WriteLine("# {0} samples", m);
+            var result = random.Sample(rand).Take(m);
+            Console.WriteLine(result.Format(" ", "{0}", p => fmt(p)));
             Console.WriteLine();
 
-            var n = 1000000;
+            Console.WriteLine("# frequency of {0} samples", n);
             var sample = random.Sample(rand).Take(n);
-            var pairs = sample.GroupBy(p => p).Select(p => new { p.Key, P = p.Count() / (double)n }).OrderBy(p => p.P);
-            Console.WriteLine(pairs.Format(Environment.NewLine, "{0,2} {1,7:P2}", p => fmt(p.Key), p => p.P));
+            var pairs = sample
+                .GroupBy(p => p)
+                .Select(p => new { p.Key, P = p.Count() / (double)n })
+                .OrderBy(p => p.P);
+            Console.WriteLine(pairs.Format(Environment.NewLine, "{0,4} {1,7:P2}", p => fmt(p.Key), p => p.P));
             Console.WriteLine();
         }
     }
