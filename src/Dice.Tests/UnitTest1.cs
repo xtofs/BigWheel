5486ad1fa69ccf1aae572923eef28db3d4e7d5a8
Christof Sprenger <christof_sprenger@hotmail.com>
2016-01-17 17:30:00 -0800

    rename test methods

--- src/Dice.Tests/UnitTest1.cs
+++ src/Dice.Tests/UnitTest1.cs
@@ -7,14 +7,14 @@ namespace Dice.Tests
 {
     public class UnitTest1
     {
-        [Theory, MemberData("ReductionTestsData")]
+        [Theory, MemberData("ReductionTestData")]
         public void ReductionTests(Rational actual, Rational expected)
         {
             Assert.Equal(expected.Numerator, actual.Numerator);
             Assert.Equal(expected.Denominator, actual.Denominator);
         }
 
-        public static readonly IEnumerable<object[]> ReductionTestsData = new[] {
+        public static readonly IEnumerable<object[]> ReductionTestData = new[] {
             new object[] {new Rational(1, 2), new Rational(1, 2) },
             new object[] {new Rational(-1, -2), new Rational(1, 2) },
             new object[] {new Rational(2, 4), new Rational(1, 2) },
@@ -25,14 +25,14 @@ namespace Dice.Tests
             new object[] {new Rational(1, 2) + new Rational(1, 3), new Rational(5, 6) }
         };
 
-        [Theory, MemberData("ConversionTestsData")]
-        public void ConversionTests(Rational data, Decimal expected)
+        [Theory, MemberData("ConversionTestData")]
+        public void ConversionTests(Rational data, decimal expected)
         {
             var actual = Convert.ToDecimal(data);
             Assert.Equal(expected, actual);
         }
 
-        public static readonly IEnumerable<object[]> ConversionTestsData = new[] {
+        public static readonly IEnumerable<object[]> ConversionTestData = new[] {
             new object[] {new Rational(1, 3), 1/3m },
             new object[] {new Rational(1, -3), -1/3m }
         };
